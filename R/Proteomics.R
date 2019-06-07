e6d89ef8fdb744154f83cc7994a6039e04bc3410
Su Wu <wusu2014@gmail.com>
2019-06-07 18:12:18 -0400

    install new packages

--- R/Proteomics.R
+++ R/Proteomics.R
@@ -1,4 +1,11 @@
-BiocManager::install("RforProteomics", dependencies = TRUE)
+BiocManager::install("RforProteomics")
+library("ggplot2")  ## Convenient and nice plotting
+library("RColorBrewer") ## Color palettes
 library("RforProteomics")
+library("reshape2") ## Flexibly reshape data
+
+
+
 pp <- proteomicsPackages()
 display(pp)
+vignette(package = "RforProteomics")
